11ae901316aa78750608fcaa849988b92e2ccb0f
larinde <olarinde.ajai@gmail.com>
2018-05-09 10:49:46 +0100

    initial project

---
+++ invoices-api/src/main/java/com/koweg/invoices/App.java
@@ -0,0 +1,13 @@
+package com.koweg.invoices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
