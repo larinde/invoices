cf4e8d5a7432caa3aff63f52b505d42c2fc52b4e
larinde <olarinde.ajai@gmail.com>
2018-10-26 15:00:24 +0100

    update swagger contract

---
+++ invoices-repository/src/main/java/com/koweg/invoices/InvoicesRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.koweg.invoices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesRepositoryApplication.class, args);
+	}
+}
